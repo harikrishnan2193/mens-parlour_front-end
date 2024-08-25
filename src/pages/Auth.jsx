55494f981d77c3f98fbeb029723f733b50fff3ae
harikrishnan2193 <harikrishnank2193@gmail.com>
2024-08-25 17:48:09 +0530

    modified version

--- src/pages/Auth.jsx
+++ src/pages/Auth.jsx
@@ -1,18 +1,28 @@
-import React from 'react'
-import './Auth.css'
-import { Link } from 'react-router-dom'
+import React, { useEffect } from 'react';
+import './Auth.css';
+import { Link } from 'react-router-dom';
 
-function Login({ register }) {
+function Auth({ register }) {
 
-    const registerPage = register ? true : false
+    const registerPage = register ? true : false;
+
+    //useffect to restart the animation when the path change
+    useEffect(() => {
+        const loginBox = document.querySelector('.login-box');
+        if (loginBox) {
+            loginBox.classList.remove('animate');
+            void loginBox.offsetWidth;
+            loginBox.classList.add('animate');
+        }
+    }, [registerPage]);
 
     return (
         <>
             <div className="container login-container">
-                <div className="row login-section d-flex">
+                <div className="row login-section d-flex pt-5">
                     <div className={`col-lg-6 col-md-12 col-sm-12 ${registerPage ? 'bg-image-large' : 'bg-image'} d-flex justify-content-center align-items-center flex-column text-white`}>
                         <h4 className="text-center mb-3">
-                            {registerPage ? "Alredy have an account?" : "Don't have an account?"}
+                            {registerPage ? "Already have an account?" : "Don't have an account?"}
                         </h4>
                         <p className="text-center">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque illum quos, id dicta dolore iure consequuntur accusantium quibusdam optio deserunt, corporis sapiente qui. Amet dignissimos tempore dolor adipisci, natus id.</p>
                         {registerPage ?
@@ -21,7 +31,7 @@ function Login({ register }) {
                             <Link to={'/register'}><button className='btn btn-outline-light mt-2 rounded-pill '>LOGIN</button></Link>
                         }
                     </div>
-                    <div className="col-lg-6 col-md-12 col-sm-12 login-box">
+                    <div className="col-lg-6 col-md-12 col-sm-12 login-box animate">
                         <h2>
                             {registerPage ? "Sign Up" : "Login"}
                         </h2>
@@ -41,16 +51,15 @@ function Login({ register }) {
                         </div>
                         {
                             registerPage ?
-                            <button type="submit" className="login-btn">SignUp</button>
-                            :
-                            <button type="submit" className="login-btn">Login</button>
+                                <button type="submit" className="login-btn">Sign Up</button>
+                                :
+                                <button type="submit" className="login-btn">Login</button>
                         }
                     </div>
                 </div>
             </div>
         </>
-
-    )
+    );
 }
 
-export default Login
+export default Auth;
